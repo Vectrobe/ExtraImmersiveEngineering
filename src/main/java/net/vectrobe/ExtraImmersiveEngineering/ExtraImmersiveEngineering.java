fcfcffe89596d6f1bd748d4ee07e84ef23177010
Paul Hancock <Paul.Hancock.17041993@live.com>
2017-03-26 19:06:14 +1100

    Initial commit

---
+++ src/main/java/net/vectrobe/ExtraImmersiveEngineering/ExtraImmersiveEngineering.java
@@ -0,0 +1,18 @@
+package net.vectrobe.ExtraImmersiveEngineering;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ExtraImmersiveEngineering.MODID, version = ExtraImmersiveEngineering.VERSION)
+public class ExtraImmersiveEngineering
+{
+    public static final String MODID = "ExtraImmersiveEngineering";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
